f8d4f3e76c04cf8071319196cf8549b70475ecb7
Bartłomiej Kasprzak <b_kasprzak@o2.pl>
2016-08-31 23:09:23 +0200

    Initial commit

---
+++ app/src/main/java/akademiakodu/pl/calculator/MainActivity.java
@@ -0,0 +1,27 @@
+package akademiakodu.pl.calculator;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button addButton, subtrButton, multiButton, divButton;
+    EditText number1, number2;
+    TextView score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
